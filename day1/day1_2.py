e22b22e02f01584aa4385542d05487b5a09228b7
jeff <jeffreybunschoten@hotmail.com>
2025-12-02 10:18:12 +0100

    removed: comments

--- day1/day1_2.py
+++ day1/day1_2.py
@@ -8,11 +8,5 @@ from FindPassword import FindPassword
 puzzle = PuzzleInput('/home/jeff/Documents/projects/adventOfCode/2025/day1/puzzle.txt')
 # print(puzzle.content)
 
-# The dial has N positions (N = 100)
-# You start at position 50
-# You rotate k steps (positive for right, negative for left)
-# Formule Turning Left: passes = (start + k) // N
-# Formule Turning Right: passes = ((N - 1 + start) - k) // N
-
 findpassword = FindPassword(puzzle.content, method='0x434C49434B')
 print(findpassword.calculatePassword())
